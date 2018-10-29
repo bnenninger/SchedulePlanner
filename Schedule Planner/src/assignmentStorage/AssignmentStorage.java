21fcc52d8e82f649eb66bf918cefa51e6ff7ce34
BrendanN <37684959+bnenninger@users.noreply.github.com>
2018-10-28 23:21:20 -0500

    Adds assignment editing and creation

--- Schedule Planner/src/assignmentStorage/AssignmentStorage.java
+++ Schedule Planner/src/assignmentStorage/AssignmentStorage.java
@@ -5,6 +5,8 @@ import java.time.LocalDate;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
+import controller.AssignmentEditUI;
+
 //TODO make this implementation faster
 //currently designed to be function
 public class AssignmentStorage implements Serializable {
@@ -12,6 +14,7 @@ public class AssignmentStorage implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = -296259272907653448L;
+	
 	private SortedSet<Assignment> assignments;
 	
 	public AssignmentStorage() {
@@ -24,6 +27,21 @@ public class AssignmentStorage implements Serializable {
 	
 	public void add(Assignment a) {
 		assignments.add(a);
+	}
+	
+	public void edit(Assignment a) {
+		AssignmentEditUI editor = new AssignmentEditUI(a);
+		editor.showAndReturn();
+		remove(a);
+		add(a);
+	}
+	
+	public void createNewAssignment() {
+		AssignmentEditUI editor = new AssignmentEditUI();
+		Assignment newAssignment = editor.showAndReturn();
+		if(newAssignment != null) {
+			add(newAssignment);
+		}
 	}
 	
 	public void remove(Assignment a) {
